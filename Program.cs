bd57a27f0f55d0fd0b2e59ac76883404d57baf67
Katerina-ops-18 <61978136+Katerina-ops-18@users.noreply.github.com>
2021-04-15 14:40:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = MyClass<bool>.FactoryMethod();
+            var y = MyClass<decimal>.FactoryMethod();
+            Console.WriteLine(x.GetType().Name);
+            Console.WriteLine(y.GetType().Name);
+        }
+    }
+    class MyClass<T>
+        where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+}
